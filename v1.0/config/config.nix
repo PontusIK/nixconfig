93bfa147299b45161b353aef421c3b8bd6b8f27b
PontusIK <pontus.kavrestad@gmail.com>
2025-04-10 16:26:52 +0200

    added neovim options

--- v1.0/config/config.nix
+++ v1.0/config/config.nix
@@ -96,6 +96,14 @@
     neovim = {
       enable = true;
       defaultEditor = true;
+      extraLuaConfig = ''
+        vim.opt.tabstop = 2
+	vim.opt.shiftwidth = 2
+	vim.opt.expandtab = true
+
+	vim.opt.number = true
+	vim.opt.relativenumber = false
+      '';
     };
     git.enable = true;
   };

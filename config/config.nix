c862c7df8753d9fca9601f09870bdb17ca5cf821
PontusIK <pontus.kavrestad@gmail.com>
2025-04-10 17:55:19 +0200

    changed neovim option

--- config/config.nix
+++ config/config.nix
@@ -96,13 +96,15 @@
     neovim = {
       enable = true;
       defaultEditor = true;
-      extraLuaConfig = ''
-        vim.opt.tabstop = 2
-	vim.opt.shiftwidth = 2
-	vim.opt.expandtab = true
+      extraConfig = ''
+        lua << EOF
+          vim.opt.tabstop = 2
+	  vim.opt.shiftwidth = 2
+	  vim.opt.expandtab = true
 
-	vim.opt.number = true
-	vim.opt.relativenumber = false
+	  vim.opt.number = true
+	  vim.opt.relativenumber = false
+	EOF
       '';
     };
     git.enable = true;

88c1527986776554e8f412ca2141b9c46f800e12
PontusIK <pontus.kavrestad@gmail.com>
2025-04-23 11:17:28 +0200

    configure nvf

--- config/nvf.nix
+++ config/nvf.nix
@@ -4,6 +4,24 @@
 
   programs.nvf = {
     enable = true;
+
+    settings.vim = {
+
+      options = {
+        tabstop = 2;
+        shiftwidth = 2;
+        wrap = false;
+      };
+
+      lineNumberMode = "number";
+
+      languages = {
+        enableLSP = true;
+        enableTreesitter = true;
+
+        nix.enable = true;
+      };
+    };
   };
 
 }

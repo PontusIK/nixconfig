981e7beb302d462ce49ceb7f9f926488b3697ee5
PontusIK <pontus.kavrestad@gmail.com>
2025-04-23 09:29:20 +0200

    replaced cosmic with budgie

--- config/config.nix
+++ config/config.nix
@@ -5,11 +5,11 @@
 { pkgs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware.nix
-      ./cosmic.nix
-    ];
+  imports = [
+    ./hardware.nix
+#    ./cosmic.nix
+    ./budgie.nix
+  ];
 
   # Bootloader.
   # vm only, change to appropriate when on host
@@ -84,14 +84,10 @@
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.pontus = {
     isNormalUser = true;
     description = "pontus";
     extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
-    #  thunderbird
-    ];
   };
   
   home-manager = {
@@ -105,14 +101,7 @@
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
-    kitty
-    wofi
-  ];
+  environment.systemPackages = with pkgs; [];
 
   nix.gc = {
     automatic = true;
